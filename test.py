40aa942818c8b739b0c721bbd23e1c5d35ac312f
akuppal <akuppal@ucsd.edu>
2025-04-27 18:19:03 -0700

    With plot and kmeans

--- test.py
+++ test.py
@@ -8,7 +8,9 @@ app = marimo.App()
 def simple_ui():
     import marimo as mo
     import pandas as pd
-    return mo, pd
+    from sklearn.cluster import KMeans
+    import altair as alt
+    return KMeans, alt, mo, pd
 
 
 @app.cell
@@ -20,8 +22,45 @@ def _(pd):
 
 
 @app.cell
+def _(KMeans, data_df):
+    #Clustering - Number of clusters
+    kmeans = KMeans(n_clusters=5, random_state=42)
+    clusters = kmeans.fit_predict(data_df[['UMAP_1', 'UMAP_2']])
+    data_df['clusters'] = clusters    
+    return clusters, kmeans
+
+
+@app.cell
+def _(alt):
+    def scatter(df, pan = False):
+        selection = alt.selection_interval(bind='scales', translate=pan, zoom=True)
+        return (alt.Chart(df)
+            .mark_circle()
+            .encode(
+                x=alt.X("UMAP_1:Q"),
+                y=alt.Y("UMAP_2:Q"),
+                color=alt.Color("clusters:N")
+            )
+            ).add_params(selection)
+    return (scatter,)
+
+
+@app.cell
+def _(mo):
+    checkbox = mo.ui.checkbox(label="Toggle Pan")
+    checkbox
+    return (checkbox,)
+
+
+@app.cell
+def _(checkbox, data_df, mo, scatter):
+    chart = mo.ui.altair_chart(scatter(data_df, checkbox.value))
+    chart
+    return (chart,)
+
+
+@app.cell
 def _():
-        
     return
 
 
